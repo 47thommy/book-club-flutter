1b1a88a5e2cf73626172fd42818718ae5ef46e5a
Thomas <47thommy@gmail.com>
2023-05-18 17:13:48 +0300

    added sign in and sign up  pages

---
+++ client/lib/login/login.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class LoginForm extends StatefulWidget {
+  final VoidCallback onSignupButtonPressed;
+
+  const LoginForm({super.key, required this.onSignupButtonPressed});
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(
+                    child: Text("GET READY TO REWIRE YOUR BRAIN",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(child: Image.asset("assets/books.png"))
+                ],
+              ),
+              const Text("Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email Address'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your email address';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+                onSaved: (value) {},
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                  }
+                },
+                child: const Text('Login'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Do not have an account? "),
+                  TextButton(
+                    onPressed: widget.onSignupButtonPressed,
+                    child: const Text('Sign Up'),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
+import 'login/login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: LoginPage(),
+  ));
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Book Club")),
-      ),
-    );
-  }
-}
-
