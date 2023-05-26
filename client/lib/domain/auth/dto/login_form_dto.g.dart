27df077f3b3a02a5183864df0e9dacdfb57ccc56
Leul Wujira <meraf000@gmail.com>
2023-05-26 04:12:10 +0300

    Add login and registration dto

---
+++ client/lib/domain/auth/dto/login_form_dto.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'login_form_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_LoginFormDto _$$_LoginFormDtoFromJson(Map<String, dynamic> json) =>
+    _$_LoginFormDto(
+      email: json['email'] as String,
+      password: json['password'] as String,
+    );
+
+Map<String, dynamic> _$$_LoginFormDtoToJson(_$_LoginFormDto instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'password': instance.password,
+    };
