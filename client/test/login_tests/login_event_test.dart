580b54864b95b4d3f2ae5e4610426ea3523e1750
47thommy <47thommy@gmail.com>
2023-06-01 19:35:04 +0300

    added login unit test

---
+++ client/test/login_tests/login_event_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:client/application/login/login_event.dart';
+import 'package:client/infrastructure/auth/dto/dto.dart';
+
+void main() {
+  group('LoginRequested', () {
+    test('toString() returns correct string representation', () {
+      const email = 'test@example.com';
+      const password = 'password';
+      const form = LoginFormDto(email: email, password: password);
+      const event = LoginRequested(form);
+
+      expect(event.toString(), 'Login requested { email: $form }');
+    });
+  });
+}
