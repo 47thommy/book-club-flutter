de98a60ac4b07c4405726f1512e4d29bcb529f91
betabravahC:/Users/betty/AppData/Local/Programs/Python/Python310/python.exe <betabravah@gmail.com>
2023-06-01 10:53:59 +0300

    Add Book and Reading List Dto

--- client/lib/common/constants.dart
+++ client/lib/common/constants.dart
@@ -1,4 +1,4 @@
-const apiUrl = 'http://192.168.48.163:3000';
+const apiUrl = 'http://192.168.43.226:3000';
 // const apiUrl = 'http://192.168.1.8:3000';
 // const apiUrl = 'http://192.168.138.163:3000';
 // const apiUrl = 'http://192.168.43.226:3000';
