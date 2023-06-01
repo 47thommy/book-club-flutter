e657238632c575f7c4e803e69441e1fea21ae1ed
RihanaE <rihanaersanu@gmail.com>
2023-06-01 17:05:43 +0300

    group_mapper

---
+++ client/lib/infrastructure/group/model/group_mapper.dart
@@ -0,0 +1,37 @@
+import 'package:client/domain/group/group.dart';
+import 'package:client/domain/poll/poll.dart';
+import 'package:client/domain/role/role.dart';
+import 'package:client/infrastructure/group/model/group_dto.dart';
+import 'package:client/infrastructure/poll/poll.dart';
+import 'package:client/infrastructure/role/role.dart';
+import 'package:client/infrastructure/user/dto/user_mapper.dart';
+
+extension GroupMapper on Group {
+  GroupDto toGroupDto() {
+    return GroupDto(
+      id: id,
+      name: name,
+      description: description,
+      imageUrl: imageUrl,
+      creator: creator.toUserDto(),
+      members: members.map((user) => user.toUserDto()).toList(),
+      roles: roles.map<RoleDto>((role) => role.toRoleDto()).toList(),
+      polls: polls.map<PollDto>((poll) => poll.toPollDto()).toList(),
+    );
+  }
+}
+
+extension GroupDtoMapper on GroupDto {
+  Group toGroup() {
+    return Group(
+      id: id,
+      name: name,
+      description: description,
+      imageUrl: imageUrl,
+      creator: creator.toUser(),
+      members: members.map((userDto) => userDto.toUser()).toList(),
+      roles: roles.map<Role>((roleDto) => roleDto.toRole()).toList(),
+      polls: polls.map<Poll>((pollDto) => pollDto.toPoll()).toList(),
+    );
+  }
+}
