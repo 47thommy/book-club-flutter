7f5e006db4a55a7884ed7effa808dae75d2891a2
betabravahC:/Users/betty/AppData/Local/Programs/Python/Python310/python.exe <betabravah@gmail.com>
2023-06-01 10:15:09 +0300

    Add Book Route

---
+++ server/src/routes/book.routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { loginRequired } = require("../middlewares/auth");
+
+
+const bookController = require("../controllers/book.controller");
+
+const router = express.Router();
+
+router.post(
+    "/",
+    loginRequired,
+    bookController.createBook
+);
+
+router.get(
+    "/:id",
+    loginRequired,
+    bookController.getBook
+);
+
+router.patch(
+    "/:id",
+    loginRequired,
+    bookController.updateBook
+);
+
+router.delete(
+    "/:id",
+    loginRequired,
+    bookController.deleteBook
+);
+
+module.exports = router;
