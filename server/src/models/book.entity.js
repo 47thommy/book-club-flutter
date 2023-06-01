81a9955bae2ce4db27581763530bf689853d12ae
bintnesro <habibanesro9040@gmail.com>
2023-06-02 05:16:27 +1300

    modify book entity

--- server/src/models/book.entity.js
+++ server/src/models/book.entity.js
@@ -2,25 +2,25 @@ const BookSchema = require("typeorm").EntitySchema;
 
 const Book = new BookSchema({
     name: "Book",
-    
+
     columns: {
         id: {
-            primary: true, 
-            type: "int", 
+            primary: true,
+            type: "int",
             generated: true,
-        }, 
+        },
 
         title: {
             type: "varchar",
-        }, 
+        },
 
         author: {
             type: "varchar",
-        }, 
+        },
 
         description: {
             type: "varchar",
-        }, 
+        },
 
         pageCount: {
             type: "int",
@@ -29,8 +29,18 @@ const Book = new BookSchema({
         genre: {
             type: "varchar",
         },
-    }
 
+    },
+
+    relations: {
+
+        group: {
+            target: "Group",
+            type: "one-to-one",
+            joinColumn: true,
+            eager: true,
+        },
+    }
 
 })
 
