1cb9990ca070846a4e3fc856dad2bbed0ac461ef
betabravahC:/Users/betty/AppData/Local/Programs/Python/Python310/python.exe <betabravah@gmail.com>
2023-06-01 10:12:10 +0300

    Add Book and ReadingList

--- server/src/configs/db.config.js
+++ server/src/configs/db.config.js
@@ -9,13 +9,15 @@ const {
   Role,
   Permission,
   Meeting,
+  Book,
+  ReadingList,
 } = require("../models");
 
 
 const database = new typeorm.DataSource({
   type: "sqlite",
   database: "bookclub.sqlite",
-  entities: [User, Group, Membership, Poll, Vote, Role, Permission, Meeting],
+  entities: [User, Group, Membership, Poll, Vote, Role, Permission, Meeting, Book, ReadingList],
   synchronize: true,
 });
 
